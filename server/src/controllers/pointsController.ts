d5a2f2b9e9abb2d9859ed2000dd292537b0504c3
GabsLoops <gabslopes34@gmail.com>
2020-06-07 18:44:12 -0300

    commit feature upload image

--- server/src/controllers/pointsController.ts
+++ server/src/controllers/pointsController.ts
@@ -15,8 +15,15 @@ class PointsController {
         .where('uf', String(uf))
         .select('points.*')
         .distinct()
+
+        const serializedPpints = points.map(point => {
+            return {
+                ...point,
+                image_url: `http://192.168.15.4:3000/uploads/${point.image}`
+            }
+        })
      
-        return response.json(points)
+        return response.json(serializedPpints)
     }
 
     async show(request: Request, response: Response){
@@ -31,7 +38,11 @@ class PointsController {
         .where('points_items.point_id', id)
         .select('title')
 
-        return response.json({ point, items })
+        const serializedPoint = {            
+            ...point,
+            image_url: `http://192.168.15.4:3000/uploads/${point.image}`            
+        }
+        return response.json({ serializedPoint, items })
 
     }
     async create(request: Request, response: Response) {
@@ -52,7 +63,7 @@ class PointsController {
       
          const trx = await knex.transaction()
          const point = {
-            image: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60' ,
+            image: request.file.filename ,
             name,
             email,
             whatsapp,
@@ -63,7 +74,10 @@ class PointsController {
          }
          const insertedIds = await trx('points').insert(point)
          const point_id = insertedIds[0]
-         const pointsItems = items.map((item_id: number )=> {
+         const pointsItems = items
+         .split(',')
+         .map((item: string) => Number(item.trim()))
+         .map((item_id: number )=> {
             return {
                item_id,
                point_id,
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,15 @@
 import express from 'express'
-
+import multer from 'multer'
+import { celebrate, Joi } from 'celebrate'
 import PointsController from './controllers/pointsController'
 import ItemsController from './controllers/itemsController'
+import multerConfig from '../src/config/multer'
 
 const router = express.Router()
 const pointsController = new PointsController()
 const itemsController = new ItemsController
+const upload = multer(multerConfig)
+
 
 router.get('/ping', (request, response) => {
    return response.json('pong')
@@ -17,6 +21,24 @@ router.get('/points', pointsController.index)
 
 router.get('/points/:id', pointsController.show)
 
-router.post('/points', pointsController.create)
+router.post(
+   '/points', 
+   upload.single('image'),
+   celebrate({
+      body: Joi.object().keys({
+         name: Joi.string().required(),
+         email: Joi.string().required().email(),
+         whatsapp: Joi.number().required(),
+         latitude: Joi.number().required(),
+         longitude: Joi.number().required(),
+         items: Joi.string().required(),
+         city: Joi.string().required(),
+         uf: Joi.string().required().max(2)
+
+      })
+   }, {
+      abortEarly: false
+   }),
+   pointsController.create)
 
 export default router;
